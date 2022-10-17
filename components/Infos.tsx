73b23c44429c05841f4c0973c88961358fd55cd3
Matheus Tavares <tavareszmatheusz@gmail.com>
2022-10-16 22:46:53 -0300

    Alteração no sobre mim

--- components/Infos.tsx
+++ components/Infos.tsx
@@ -4,7 +4,7 @@ const Infos = (): JSX.Element => {
         <div className="flex md:flex-row flex-col justify-evenly">
             <div className="flex flex-col text-center md:text-left justify-center">
                 <h2 className="py-8 text-black font-['Poppins'] text-xl font-bold">Sobre mim:</h2>
-                <p className="text-black font-['Poppins'] text-s max-w-[650px]">Me chamo Matheus, tenho 18 anos, tive meu primeiro contato com a programação muito cedo com o Java em 2016, um tempo depois em 2019 comecei a estudar o JavaScript e no final do ano de 2020 comecei a me dedicar ao desenvolvimento web. Por enquanto minhas únicas experiências foram com free-lancer porém estou em busca da primeira expêriencia profissional.</p>
+                <p className="text-black font-['Poppins'] text-s max-w-[650px]">Tive meu primeiro contato na programação em 2016 com o Java, depois em 2019 com JavaScript e no final de 2021 comecei a estudar sobre desenvolvimento web desenvolvendo tanto o front-end quanto o back-end. Tenho experiência com Java, MySql, Node.JS, Next.js, Express.js, MongoDB.</p>
             </div>
             <div className="flex flex-col  text-center md:text-left justify-center">
                 <h2 className="py-8 text-black font-['Poppins'] text-xl font-bold">Experiência:</h2>
