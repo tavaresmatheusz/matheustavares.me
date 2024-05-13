168bf306e665051e05f8253f4d9ec08aa58e623e
Matheus Tavares <tavareszmatheusz@gmail.com>
2024-05-13 13:12:14 -0300

    commit

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -7,6 +7,27 @@ const Portfolio = (): JSX.Element => {
 
         <div className="flex flex-row justify-evenly flex-wrap py-[80px] w-[100%] gap-y-[20px] gap-x-[80px]">
 
+        <div className="flex md:flex-row flex-col shadow-[1px_0px_4px_4px_rgba(0,0,0,0.05)] rounded-[30px]">
+            <Image src={"/sites/digiglow.png"} width={375} height={260} quality={100} className="rounded-tl-[30px] md:rounded-bl-[30px] rounded-tr-[30px] md:rounded-tr-[0px]"/>
+            <div className="box-border h-[260px] w-[353px] p-4 flex flex-col gap-y-[10px]">
+                <h2 className="font-['Poppins'] text-xl text-black">digiglow.com.br</h2>
+                <div className="flex flex-row gap-x-[5px] w-[239px]">
+                    <h2 className="font-['Poppins'] text-s font-bold text-black">Stacks:</h2>
+                    <p className="font-['Poppins'] text-s text-black min-w-[100%]">JavaScript, React, Next.js, MongoDB</p>
+                </div>
+                <div className="flex flex-row gap-x-[5px] w-[239px]">
+                    <h2 className="font-['Poppins'] text-s font-bold text-black">Detalhes:</h2>
+                    <p className="font-['Poppins'] text-s text-black min-w-[100%]">Agência de sites.</p>
+                </div>
+
+                <div className="flex flex-row gap-x-[30px] w-[239px] py-[15px]">
+                    <a className="bg-none rounded-[30px] shadow-[1px_0px_4px_4px_rgba(0,0,0,0.15)] py-[8px] px-[15px] font-bold text-s text-black opacity-40 hover:cursor-not-allowed">REPOSITORIO</a>
+                        <a href="https://digiglow.com.br" className="bg-black rounded-[30px] shadow-[1px_0px_4px_4px_rgba(0,0,0,0.15)] py-[8px] px-[15px] font-bold text-s text-white">DEMONSTRAÇÃO</a>
+                </div>
+
+            </div>
+        </div>
+
             <div className="flex md:flex-row flex-col shadow-[1px_0px_4px_4px_rgba(0,0,0,0.05)] rounded-[30px]">
                 <Image src={"/sites/loupsite.png"} width={375} height={260} quality={100} className="rounded-tl-[30px] md:rounded-bl-[30px] rounded-tr-[30px] md:rounded-tr-[0px]"/>
                 <div className="box-border h-[260px] w-[353px] p-4 flex flex-col gap-y-[10px]">
